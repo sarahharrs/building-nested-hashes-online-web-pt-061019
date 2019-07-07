a3550aa0f6264e24feb2429dc7ff92479bf757fb
Sarah Harrs <sarahharrs@gmail.com>
2019-07-07 20:58:43 +0000

    Done.

--- lib/bonus.rb
+++ lib/bonus.rb
@@ -1,5 +1,5 @@
 def bonus
-  epic_tragedy = {
+  epic_tragedy ={
    :montague => {
       :patriarch => {name: "Lord Montague", age: "53"},
       :matriarch => {name: "Lady Montague", age: "54"},
@@ -19,11 +19,12 @@ def bonus
       ]
    }
   }
-
-  #code your solution here:
-
+  epic_tragedy[:capulet] [:heroine] [:status] = "dead"
+ 
+ epic_tragedy
   
+  epic_tragedy[:montague] [:hero] [:status] = "dead"
+ 
+ epic_tragedy
 
-  #Don't touch the following line! The `bonus` method must return our newly modified epic tragedy hash
-  epic_tragedy
 end
